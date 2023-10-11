4d9669f31de67ca052f80724faaa15bd6302faf3
TathastuCodes <116951650+TathastuCodes@users.noreply.github.com>
2023-10-11 19:24:51 +0530

    adding project files

---
+++ fargatecdk/services/fargateawsdemo/fargateawsdemo/ConsoleHostedService.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Serilog;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace fargateawsdemo
+{
+    internal sealed class ConsoleHostedService : IHostedService
+    {
+        private readonly ILogger _logger;
+        private readonly IConfiguration _configuration;
+        private readonly IHostApplicationLifetime _appLifetime;
+
+        public ConsoleHostedService(
+            ILogger logger,
+            IConfiguration configuration,
+            IHostApplicationLifetime appLifetime)
+        {
+            _logger = logger;
+            _configuration = configuration;
+            _appLifetime = appLifetime;
+        }
+
+        public Task StartAsync(CancellationToken cancellationToken)
+        {
+            _logger.Debug("Starting");
+
+            _appLifetime.ApplicationStarted.Register(() =>
+            {
+                Task.Run(() =>
+                {
+                    try
+                    {
+                        RunConsoleApp();
+                    }
+                    catch (Exception ex)
+                    {
+                        _logger.Fatal(ex, "Unhandled exception!");
+                    }
+                    finally
+                    {
+                        _appLifetime.StopApplication();
+                    }
+                }, cancellationToken);
+            });
+
+            return Task.CompletedTask;
+        }
+
+        private void RunConsoleApp()
+        {
+
+            _logger.Information("batch fargate app invoked");
+        }
+
+        public Task StopAsync(CancellationToken cancellationToken) => Task.CompletedTask;
+    }
+}
---
+++ fargatecdk/services/fargateawsdemo/fargateawsdemo/Program.cs
@@ -0,0 +1,26 @@
+﻿using fargateawsdemo;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Serilog;
+
+
+var environmentName = Environment.GetEnvironmentVariable("DOTNET_ENVIRONMENT") ?? "DEV";
+await Host.CreateDefaultBuilder()
+.UseEnvironment(environmentName)
+    //.ConfigureAppConfiguration((_, builder) =>
+    //{
+    //    builder.AddJsonFile("appsettings.json")
+    //        .AddJsonFile($"appsettings.{environmentName}.json");
+    //    builder.Build();
+    //})
+    .ConfigureServices((_, services) =>
+    {
+        services.AddHostedService<ConsoleHostedService>();
+
+        services.AddSingleton<ILogger>(new LoggerConfiguration().WriteTo.Console().CreateLogger());
+    })
+    .RunConsoleAsync();
+
+
+
